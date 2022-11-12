6e0b8d7ea5c6f92694345c1947af5fb5be71d8a6
Thomas Kenny <thomaskenny9734@gmail.com>
2022-11-11 22:02:09 -0500

    added coffee mug with steam wafting off

--- spinning-cube/src/App.js
+++ spinning-cube/src/App.js
@@ -4,20 +4,44 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='container'>
+        <div className='plate'>
+        </div>
+        <div className='cup'>
+          <div className='top'>
+            <div className='vapour'>
+              <span style={{'--i': 18}}></span>
+              <span style={{'--i': 3}}></span>
+              <span style={{'--i': 15}}></span>
+              <span style={{'--i': 7}}></span>
+              <span style={{'--i': 12}}></span>
+              <span style={{'--i': 16}}></span>
+              <span style={{'--i': 11}}></span>
+              <span style={{'--i': 20}}></span>
+              <span style={{'--i': 5}}></span>
+              <span style={{'--i': 19}}></span>
+              <span style={{'--i': 4}}></span>
+              <span style={{'--i': 8}}></span>
+              <span style={{'--i': 10}}></span>
+              <span style={{'--i': 2}}></span>
+              <span style={{'--i': 13}}></span>
+              <span style={{'--i': 1}}></span>
+              <span style={{'--i': 9}}></span>
+              <span style={{'--i': 6}}></span>
+              <span style={{'--i': 14}}></span>
+              <span style={{'--i': 17}}></span>
+            </div>
+            <div className='circle'>
+              <div className='tea'>
+
+              </div>
+            </div>
+          </div>
+          <div className='handle'> 
+
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
